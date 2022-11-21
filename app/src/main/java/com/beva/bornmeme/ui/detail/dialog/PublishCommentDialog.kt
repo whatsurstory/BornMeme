19f261f5e33f593fdba4228f3bb3e5f6418a1547
YiTingLing <whatsurstory2016@gmail>
2022-11-22 00:38:01 +0800

    adjusted the comment dialog

--- app/src/main/java/com/beva/bornmeme/ui/detail/dialog/PublishCommentDialog.kt
+++ app/src/main/java/com/beva/bornmeme/ui/detail/dialog/PublishCommentDialog.kt
@@ -1,22 +1,27 @@
 package com.beva.bornmeme.ui.detail.dialog
 
-import android.content.Intent
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Context.INPUT_METHOD_SERVICE
+import android.content.DialogInterface
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
 import android.view.animation.AnimationUtils
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatDialogFragment
-import androidx.core.widget.addTextChangedListener
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.DialogFragment
 import androidx.navigation.fragment.findNavController
-import com.beva.bornmeme.MainActivity
 import com.beva.bornmeme.R
 import com.beva.bornmeme.databinding.DialogCommentBinding
 import com.google.android.material.snackbar.Snackbar
 import timber.log.Timber
+
 
 class PublishCommentDialog: AppCompatDialogFragment() {
 //TODO: take the data to viewModel -> observe the argument -> complete the post comment with taking user data
@@ -25,6 +30,7 @@ class PublishCommentDialog: AppCompatDialogFragment() {
     private lateinit var postId: String
     private lateinit var parentId: String
 
+    @SuppressLint("ServiceCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setStyle(DialogFragment.STYLE_NO_FRAME, R.style.PublishDialog)
@@ -32,6 +38,43 @@ class PublishCommentDialog: AppCompatDialogFragment() {
         binding.layoutPublish.startAnimation(AnimationUtils.loadAnimation(context, R.anim.slide))
         viewModel = PublishViewModel()
         binding.dialog = this
+        binding.editPublishContent.focusAndShowKeyboard()
+
+    }
+
+    fun View.focusAndShowKeyboard() {
+        /**
+         * This is to be called when the window already has focus.
+         */
+        fun View.showTheKeyboardNow() {
+            if (isFocused) {
+                post {
+                    // We still post the call, just in case we are being notified of the windows focus
+                    // but InputMethodManager didn't get properly setup yet.
+                    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+                }
+            }
+        }
+
+        requestFocus()
+        if (hasWindowFocus()) {
+            // No need to wait for the window to get focus.
+            showTheKeyboardNow()
+        } else {
+            // We need to wait until the window gets focus.
+            viewTreeObserver.addOnWindowFocusChangeListener(
+                object : ViewTreeObserver.OnWindowFocusChangeListener {
+                    override fun onWindowFocusChanged(hasFocus: Boolean) {
+                        // This notification will arrive just before the InputMethodManager gets set up.
+                        if (hasFocus) {
+                            this@focusAndShowKeyboard.showTheKeyboardNow()
+                            // It’s very important to remove this listener once we are done.
+                            viewTreeObserver.removeOnWindowFocusChangeListener(this)
+                        }
+                    }
+                })
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater,
@@ -49,7 +92,7 @@ class PublishCommentDialog: AppCompatDialogFragment() {
             viewModel.getUserName(parentId, binding)
         }
 
-
+        
         binding.buttonPublish.setOnClickListener {
 
             if (binding.editPublishContent.text.isNullOrEmpty()) {
@@ -68,4 +111,6 @@ class PublishCommentDialog: AppCompatDialogFragment() {
 
         return binding.root
     }
+
 }
+
