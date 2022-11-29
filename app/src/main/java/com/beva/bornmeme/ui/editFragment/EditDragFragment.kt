e87ac416bbf2bf33a86e17fa3ea8cf701a0887dd
YiTingLing <whatsurstory2016@gmail>
2022-11-29 13:50:53 +0800

    fixed the waterfall height is always same

--- app/src/main/java/com/beva/bornmeme/ui/editFragment/EditDragFragment.kt
+++ app/src/main/java/com/beva/bornmeme/ui/editFragment/EditDragFragment.kt
@@ -1,39 +1,24 @@
 package com.beva.bornmeme.ui.editFragment
 
 import android.annotation.SuppressLint
-import android.graphics.Matrix
-import android.graphics.PointF
 import android.os.Bundle
-import android.util.FloatMath
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
+import android.view.View.OnTouchListener
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.RelativeLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import com.beva.bornmeme.databinding.FragmentDragEditBinding
-import timber.log.Timber
-import java.lang.Math.sqrt
-import java.lang.StrictMath.sqrt
-import kotlin.math.sqrt
 
 
-class EditDragFragment: Fragment(), View.OnTouchListener {
+class EditDragFragment: Fragment() {
 
     lateinit var binding: FragmentDragEditBinding
-    // 縮放控制
-    private val matrix: Matrix = Matrix()
-    private val savedMatrix: Matrix = Matrix()
-    // 不同状态的表示：
-    private val NONE = 0
-    private val DRAG = 1
-    private val ZOOM = 2
-    private var mode = NONE
-    // 定义第一个按下的点，两只接触点的重点，以及出事的两指按下的距离：
-    private val startPoint = PointF()
-    private var midPoint = PointF()
-    private var oriDis = 1f
+    private var xDown = 0f
+    private var yDown = 0f
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -43,119 +28,112 @@ class EditDragFragment: Fragment(), View.OnTouchListener {
 
         return binding.root
     }
+
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = FragmentDragEditBinding.inflate(layoutInflater)
-        val text = binding.dragEditText
-        var isMove = true
-//        binding.stickerImg.setOnTouchListener(this)
+//        val text = binding.dragText
+//        var isMove = true
 
+//        text.setOnClickListener{
+//         if (!isMove){
+//             Timber.d("setOnTouchListener $isMove")
+//            }
+//        }
 
-        text.setOnClickListener{
-         if (!isMove){
-             Timber.d("setOnTouchListener $isMove")
-            }
-        }
-
-        var startX = 0
-        var startY = 0
+//        var startX = 0
+//        var startY = 0
 //
-        text.setOnTouchListener { v, event ->
-            when(event.action){
+//        text.setOnTouchListener { v, event ->
+//            when(event.action){
+//
+//                MotionEvent.ACTION_DOWN -> {
+//                    startX = event.rawX.toInt()
+//                    startY = event.rawY.toInt()
+//                    isMove = false
+//                }
+//
+//                MotionEvent.ACTION_MOVE -> {
+//
+//                    val endX: Int = event.rawX.toInt()
+//                    val endY: Int = event.rawY.toInt()
+//
+//                    val spaceX = endX - startX
+//                    val spaceY = endY - startY
+//
+//                    val left = text.left + spaceX
+//                    val top = text.top + spaceY
+//                    val right = text.right + spaceX
+//                    val bottom = text.bottom + spaceY
+//
+//                    text.layout(left,top,right, bottom)
+//                    startX = endX
+//                    startY = endY
+//
+//                    if (spaceX > 5 || spaceY > 5){
+//                        isMove = true
+//                    }
+//                }
+//
+//                MotionEvent.ACTION_UP -> {
+//
+//                }
+//
+//                MotionEvent.ACTION_CANCEL -> {
+//
+//                }
+//
+//                else ->{
+//
+//                }
+//            }
+//            return@setOnTouchListener false
+//        }
 
+
+        binding.stickerImg.setOnTouchListener(OnTouchListener { v, event ->
+            when (event.actionMasked) {
                 MotionEvent.ACTION_DOWN -> {
-                    startX = event.rawX.toInt()
-                    startY = event.rawY.toInt()
-                    isMove = false
+                    xDown = event.x
+                    yDown = event.y
                 }
-
                 MotionEvent.ACTION_MOVE -> {
-
-                    val endX: Int = event.rawX.toInt()
-                    val endY: Int = event.rawY.toInt()
-
-                    val spaceX = endX - startX
-                    val spaceY = endY - startY
-
-                    val left = text.left + spaceX
-                    val top = text.top + spaceY
-                    val right = text.right + spaceX
-                    val bottom = text.bottom + spaceY
-
-                    text.layout(left,top,right, bottom)
-                    startX = endX
-                    startY = endY
-
-                    if (spaceX > 5 || spaceY > 5){
-                        isMove = true
-                    }
-                }
-
-                MotionEvent.ACTION_UP -> {
-
-                }
-
-                MotionEvent.ACTION_CANCEL -> {
-
-                }
-
-                else ->{
-
+                    val moveX: Float = event.x
+                    val moveY: Float = event.y
+                    val distanceX: Float = moveX - xDown
+                    val distanceY: Float = moveY - yDown
+                    binding.stickerImg.x = binding.stickerImg.x + distanceX
+                    binding.stickerImg.y = binding.stickerImg.y + distanceY
                 }
             }
-            return@setOnTouchListener false
-        }
+            true
+        })
+        binding.dragText.setOnTouchListener(OnTouchListener { v, event ->
+            // TODO Auto-generated method stub
+            drag(event, v)
+            false
+        })
+
     }
 
-    override fun onTouch(v: View, event: MotionEvent): Boolean {
-        val view = v as ImageView
-        when (event.action and MotionEvent.ACTION_MASK) {
+    private fun drag(event: MotionEvent?, v: View?) {
+        val params = v?.layoutParams as ConstraintLayout.LayoutParams
+
+        when (event!!.action) {
+            MotionEvent.ACTION_MOVE -> {
+                params.topMargin = event.rawY.toInt() - v.height
+                params.leftMargin = event.rawX.toInt() - v.width / 2
+                v.layoutParams = params
+            }
+            MotionEvent.ACTION_UP -> {
+                params.topMargin = event.rawY.toInt() - v.height
+                params.leftMargin = event.rawX.toInt() - v.width / 2
+                v.layoutParams = params
+            }
             MotionEvent.ACTION_DOWN -> {
-                matrix.set(v.imageMatrix)
-                savedMatrix.set(matrix)
-                startPoint.set(event.x, event.y)
-                mode = DRAG
-            }
-            MotionEvent.ACTION_POINTER_DOWN -> {
-                oriDis = distance(event)
-                if (oriDis > 10f) {
-                    savedMatrix.set(matrix)
-                    midPoint = middle(event)
-                    mode = ZOOM
-                }
-            }
-            MotionEvent.ACTION_UP, MotionEvent.ACTION_POINTER_UP -> mode = NONE
-            MotionEvent.ACTION_MOVE -> if (mode == DRAG) {
-//                 是一个手指拖动
-                matrix.set(savedMatrix)
-                matrix.postTranslate(event.x - startPoint.x, event.y - startPoint.y)
-            } else if (mode == ZOOM) {
-//                 两个手指滑动
-                val newDist = distance(event)
-                if (newDist > 10f) {
-                    matrix.set(savedMatrix)
-                    val scale: Float = newDist / oriDis
-                    matrix.postScale(scale, scale, midPoint.x, midPoint.y)
-                }
+                v.layoutParams = params
             }
         }
-        // 设置ImageView的Matrix
-        view.imageMatrix = matrix
-        return true
-    }
-
-    // 计算两个触摸点之间的距离
-    private fun distance(event: MotionEvent): Float {
-        val x = event.getX(0) - event.getX(1)
-        val y = event.getY(0) - event.getY(1)
-        return sqrt(x * x + y * y)
-    }
-
-    // 计算两个触摸点的中点
-    private fun middle(event: MotionEvent): PointF {
-        val x = event.getX(0) + event.getX(1)
-        val y = event.getY(0) + event.getY(1)
-        return PointF(x / 2, y / 2)
     }
 }
